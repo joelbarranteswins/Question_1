38b9f08bde143a59ae868e5483cb28fd5eef580c
Joel Barrantes Palacios <58517994+joelbarranteswins@users.noreply.github.com>
2023-01-17 09:49:15 -0500

    changes

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -8,5 +8,11 @@ namespace WPF_LoginForm.ViewModels
 {
     public class HomeViewModel : ViewModelBase
     {
+        private string _text = "Write something";
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; OnPropertyChanged(nameof(Text)); }
+        }
     }
 }
